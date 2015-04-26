a6dd3d80288732342f99d9a4e817f977876a7791
ElderPac <ElderPac@MSI_master_race>
2015-04-26 21:03:45 +0100

    commented grafo

--- src/grafo.cpp
+++ src/grafo.cpp
@@ -7,7 +7,9 @@
 #include "grafo.h"
 
 using namespace std;
-
+/**
+ * Removes all edges leading to that vertex we want to delete
+ */
 void Vertex::removeVertex(const long long & in) {
 	for (size_t i = 0; i < adj.size(); i++) {
 		if (*(adj[i].getDest()) == in) {
@@ -16,30 +18,42 @@ void Vertex::removeVertex(const long long & in) {
 		}
 	}
 }
-
+/**
+ * Equal operator for vertices
+ */
 bool operator==(Vertex lhs, Vertex rhs) {
 	if (lhs.getId() == rhs.getId())
 		return true;
 	else
 		return false;
 }
-
+/**
+ * Vertex constructor with only the id
+ */
 Vertex::Vertex(long long in) :
 		id(in), visited(false), lat(0), lon(0), x(0), y(0) {
 }
-
+/**
+ * Vertex Constructor with all the information
+ */
 Vertex::Vertex(long long in, double lat, double lon, double x, double y) :
 		id(in), visited(false), lat(lat), lon(lon), x(x), y(y) {
 }
-
+/**
+ * Adds an edge to a vertex's edge vector
+ */
 void Vertex::addEdge(Edge e) {
 	adj.push_back(e);
 }
-
+/**
+ * Edge constructor with the destiny vertex and its weight
+ */
 Edge::Edge(Vertex*d, double w) : //TODO check if id should be 0
 		dest(d), weight(w), id(0) {
 }
-
+/**
+ * Edge constructor with all the information
+ */
 Edge::Edge(Vertex *orig, Vertex*d, string name, double w, long long id) {
 	this->name = name;
 	this->dest = d;
@@ -47,7 +61,9 @@ Edge::Edge(Vertex *orig, Vertex*d, string name, double w, long long id) {
 	this->weight = w;
 	this->id = id;
 }
-
+/**
+ * returns a vector with Vertices when a width search is performed from a Vertex
+ */
 vector<long> Graph::bfs(Vertex * v) const {
 	vector<long> answer;
 	Vertex *check;
@@ -67,7 +83,9 @@ vector<long> Graph::bfs(Vertex * v) const {
 
 	return answer;
 }
-
+/**
+ * returns a vector with Vertices when a depth search is performed
+ */
 vector<long> Graph::dfs(vector<Vertex*> vec) const {
 
 	vector<long> answer;
@@ -89,83 +107,123 @@ void Graph::dfs(vector<long> &vec, Vertex* v) const {
 		}
 	}
 }
-
+/**
+ * returns the max Latitude of the graph
+ */
 double Graph::getMaxLat() const {
 	return maxLat;
 }
-
+/**
+ * sets the max Latitude of the graph
+ */
 void Graph::setMaxLat(double maxLat) {
 	this->maxLat = maxLat;
 }
-
+/**
+ * returns the max Longitude of the graph
+ */
 double Graph::getMaxLon() const {
 	return maxLon;
 }
-
+/**
+ * sets the max Longitude of the graph
+ */
 void Graph::setMaxLon(double maxLon) {
 	this->maxLon = maxLon;
 }
-
+/**
+ * returns the min Latitude of the graph
+ */
 double Graph::getMinLat() const {
 	return minLat;
 }
-
+/**
+ * sets the min Latitude of the graph
+ */
 void Graph::setMinLat(double minLat) {
 	this->minLat = minLat;
 }
-
+/**
+ * returns the min Longitude of the graph
+ */
 double Graph::getMinLon() const {
 	return minLon;
 }
-
+/**
+ * sets the min Longitude of the graph
+ */
 void Graph::setMinLon(double minLon) {
 	this->minLon = minLon;
 }
-
+/**
+ * retuns the origin of the edge (Vertex)
+ */
 Vertex* Edge::getOrig() const {
 	return orig;
 }
-
+/**
+ * sets the origin of the edge (Vertex)
+ */
 void Edge::setOrig(Vertex* orig) {
 	this->orig = orig;
 }
-
+/**
+ * return the center of the graph (X coord)
+ */
 double Graph::getCenterX() const {
 	return centerX;
 }
-
+/**
+ * sets the center of the graph (X coord)
+ */
 void Graph::setCenterX(double centerX) {
 	this->centerX = centerX;
 }
-
+/**
+ * return the center of the graph (Y coord)
+ */
 double Graph::getCenterY() const {
 	return centerY;
 }
-
+/**
+ * sets the center of the graph (Y coord)
+ */
 void Graph::setCenterY(double centerY) {
 	this->centerY = centerY;
 }
-
+/**
+ * returns the height of the graph
+ */
 double Graph::getHeight() const {
 	return height;
 }
-
+/**
+ * sets the height of the graph
+ */
 void Graph::setHeight(double height) {
 	this->height = height;
 }
-
+/**
+ * return the width of the graph
+ */
 double Graph::getWidth() const {
 	return width;
 }
-
+/**
+ * sets the width of the graph
+ */
 void Graph::setWidth(double width) {
 	this->width = width;
 }
-
+/**
+ * Vertex constructor
+ */
 Vertex::Vertex(long long in, double lat, double lon, double x, double y, double centeredX, double centeredY) :
 		id(in), visited(false), lat(lat), lon(lon), x(x), y(y), centeredX(centeredX), centeredY(centeredY) {
 }
-
+/**
+ * creates a disjoined set given a map
+ */
 void Graph::createDisjoinedSet(map<long long, Vertex*> toBeProcessed) {
 
 	map<long long, Vertex*>::iterator it = toBeProcessed.begin();
@@ -187,7 +245,9 @@ void Graph::createDisjoinedSet(map<long long, Vertex*> toBeProcessed) {
 	}
 
 }
-
+/**
+ * creates a disjoined set given a vertex and a vertex vector
+ */
 void Graph::createDisjoinedSet(Vertex *v, vector<Vertex *> &vec) {
 
 	v->setVisited(true);
@@ -206,12 +266,17 @@ void Graph::createDisjoinedSet(Vertex *v, vector<Vertex *> &vec) {
 	}
 
 }
-
+/**
+ * adds centrals to the graph
+ */
 void Graph::addCentrals() {
 	for (size_t i = 0; i < vertexSet.size(); i++) {
 		addCentralsAux(vertexSet[i], i);
 	}
 }
+/**
+ * Adds an auxiliar central
+ */
 void Graph::addCentralsAux(vector<Vertex*> &vec, const int &count) {
 
 	double incLon = maxLon - minLon * 1.0;
